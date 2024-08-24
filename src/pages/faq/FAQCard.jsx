5f95fe2d2f124fb6805463ad8a3c36007caf53a9
Akash Singh <mr.singhaksh@gmail.com>
2024-08-24 22:15:44 +0530

    feat:add feature card

---
+++ src/pages/faq/FAQCard.jsx
@@ -0,0 +1,59 @@
+/* eslint-disable react/prop-types */
+import { useState, useRef, useEffect } from "react";
+import { BsQuestionDiamondFill } from "react-icons/bs";
+import { FaPlus } from "react-icons/fa6";
+import { FaMinus } from "react-icons/fa6";
+
+const FAQCard = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [height, setHeight] = useState(0);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      setHeight(contentRef.current.scrollHeight);
+    } else {
+      setHeight(0);
+    }
+  }, [isOpen]);
+
+  return (
+    <div className="rounded-[1.25rem] shadow-sm bg-white text-[calc(1.325rem + .9vw)] font-normal">
+      <button
+        className="w-full text-left px-8 py-[1.2rem] flex justify-between items-center focus:outline-none text-[#032071]"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span className="text-lg font-medium flex items-center gap-2">
+          <BsQuestionDiamondFill /> {question}
+        </span>
+          <div className="relative w-6 h-6 flex items-center justify-center">
+            <span
+              className={`absolute text-2xl font-bold transition-all duration-300 ease-in-out transform ${
+                isOpen ? "rotate-90 opacity-0" : "rotate-0 opacity-100"
+              }`}
+            >
+              <FaPlus />
+            </span>
+            <span
+              className={`absolute text-2xl font-bold transition-all duration-300 ease-in-out transform ${
+                isOpen ? "rotate-0 opacity-100" : "-rotate-90 opacity-0"
+              }`}
+            >
+              <FaMinus />
+            </span>
+          </div>
+      </button>
+      <div
+        ref={contentRef}
+        style={{ height: `${height}px` }}
+        className="px-5 overflow-hidden transition-all duration-300 ease-in-out"
+      >
+        <p className="px-4 py-5 border-t-[1px] border-solid border-t-[#4611A7]">
+          {answer}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FAQCard;
--- src/pages/faq/index.jsx
+++ src/pages/faq/index.jsx
@@ -1,10 +1,34 @@
-import { useScroll } from "../scrollContext"
+import { useScroll } from "../scrollContext";
+import FAQCard from "./FAQCard";
+import faqJson from "./faq.json"
 
 const FAQ = () => {
-  const {faqRef} = useScroll()
+  const { faqRef } = useScroll();
   return (
-    <div ref={faqRef} className="w-full h-80 border-2 border-red">FAQ</div>
-  )
-}
+    <div ref={faqRef} className="w-full bg-[#F2F2F2] py-12">
+      <div>
+        <div className="text-center px-3">
+          <h1 className="text-h1-heading text-[#032071] mb-4">Frequently Asked Questions</h1>
+          <p className="text-[#878A9B] mb-12">
+          Our FAQ section addresses many common inquiries about fantasy gaming on MyFortune11.
+          <br />
+          If you have any additional questions or need further assistance, please don’t hesitate to reach out to our support team. 
+          </p>
+        </div>
+        <div className="px-5 flex flex-col gap-6">
+          {
+            faqJson.map((faq,index)=>(
+              <FAQCard
+              key={index}
+            question={faq.question}
+            answer={faq.answer}
+          />
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default FAQ
+export default FAQ;
